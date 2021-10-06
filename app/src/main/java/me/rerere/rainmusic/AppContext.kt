a7284b79e59264f70ff536ea48a1e79a1732acd8
jiangdashao <qq1609403959@outlook.com>
2021-10-06 18:00:23 +0800

    Initial commit

---
+++ app/src/main/java/me/rerere/rainmusic/AppContext.kt
@@ -0,0 +1,9 @@
+package me.rerere.rainmusic
+
+import android.app.Application
+
+class AppContext : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/me/rerere/rainmusic/MainActivity.kt
@@ -0,0 +1,24 @@
+package me.rerere.rainmusic
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import me.rerere.rainmusic.ui.theme.RainMusicTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RainMusicTheme {
+
+            }
+        }
+    }
+}
