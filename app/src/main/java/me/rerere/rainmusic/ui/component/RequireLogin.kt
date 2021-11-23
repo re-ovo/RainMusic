c2e710ed706ef144a2b68bf4303d55616f9f1062
re-ovo <qq1609403959@outlook.com>
2021-11-23 11:39:27 +0800

    新增RequireLogin组件

---
+++ app/src/main/java/me/rerere/rainmusic/ui/component/RequireLogin.kt
@@ -0,0 +1,37 @@
+package me.rerere.rainmusic.ui.component
+
+import androidx.compose.runtime.Composable
+import me.rerere.rainmusic.ui.local.LocalUserData
+import me.rerere.rainmusic.ui.local.UserData
+
+/**
+ * 只在登录后显示的内容
+ *
+ * @param content 内容
+ */
+@Composable
+fun RequireLoginVisible(
+    content: @Composable () -> Unit
+) {
+    val userData = LocalUserData.current
+    if(!userData.isVisitor){
+        content()
+    }
+}
+
+/**
+ * 根据是否登录执行不同的动作，通常用于 clickable() 中
+ *
+ * @param notLoginAction 未登录时执行的操作
+ * @param loginAction 已登录后执行的操作
+ */
+inline fun UserData.handle(
+    notLoginAction: () -> Unit,
+    loginAction: () -> Unit
+) {
+    if(isVisitor){
+       notLoginAction()
+    } else {
+        loginAction()
+    }
+}
