f64aec5c10cfde71380b541056945a3569e2eac6
re-ovo <qq1609403959@outlook.com>
2021-11-29 22:20:51 +0800

    移除channel注册

--- app/src/main/java/me/rerere/rainmusic/AppContext.kt
+++ app/src/main/java/me/rerere/rainmusic/AppContext.kt
@@ -1,25 +1,15 @@
 package me.rerere.rainmusic
 
 import android.app.Application
-import android.app.NotificationManager
 import android.content.Context
 import android.content.SharedPreferences
 import dagger.hilt.android.HiltAndroidApp
-import me.rerere.rainmusic.util.createNotificationChannel
 
 @HiltAndroidApp
 class AppContext : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
-
-        // 注册channel
-        createNotificationChannel(
-            channelId = "music",
-            name = "音乐播放器通知栏组件", // TODO: 本地化
-            description = "请勿禁用该通知", // TODO: 本地化
-            importance = NotificationManager.IMPORTANCE_DEFAULT
-        )
     }
 
     companion object {
